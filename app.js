74a0cbf914ef4324399e2219f9fffb40e75d9a05
Ruger <Ruger4@gmail.com>
2019-01-26 13:08:29 +0000

    progress

--- app.js
+++ app.js
@@ -1,49 +1,3 @@
-
-/*var camera, scene, renderer;
-var geometry, material, mesh;
-var frustumSize = 1000;
-
-init();
-animate();
-
-function init() {
-    //camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 10 );
-    var aspect = window.innerWidth / window.innerHeight;
-    camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000 );
-    scene.add( camera )
-
-    scene = new THREE.Scene();
-
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    material = new THREE.MeshNormalMaterial();
-
-    mesh = new THREE.Mesh( geometry, material );
-
-    mesh.position.x = 0.2;
-    mesh.position.y = 0.2;
-    scene.add( mesh );
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.getElementById("main").appendChild( renderer.domElement );
-    window.addEventListener( 'resize', onWindowResize, false );
-}
-function onWindowResize() {
-    var aspect = window.innerWidth / window.innerHeight;
-    camera.left = - frustumSize * aspect / 2;
-    camera.right = frustumSize * aspect / 2;
-    camera.top = frustumSize / 2;
-    camera.bottom = - frustumSize / 2;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function animate() {
-    requestAnimationFrame( animate );
-
-    renderer.render( scene, camera );
-}*/
-
 var container, camera, scene, raycaster, renderer;
 var frustumSize = 10;
 
@@ -67,12 +21,9 @@ function init() {
     light.position.set( 1, 1, 1 ).normalize();
     scene.add( light );
 
-    var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
-    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-    object.position.x = 0;
-    object.position.y = 0;
-    object.position.z = 0;
-    scene.add( object );
+    scene.add( new Triangle({position: new THREE.Vector3(0,0,0), color: 0xffff00}).create() );
+    scene.add( new Circle({position: new THREE.Vector3(-3,0,-1), color: 0x7E1D1D}).create() );
+    scene.add( new Square({position: new THREE.Vector3(3,0,-2), color: 0x0000FF}).create() );
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -80,6 +31,17 @@ function init() {
     container.appendChild( renderer.domElement );
 
     window.addEventListener( 'resize', onWindowResize, false );
+}
+function animate() {
+    requestAnimationFrame( animate );
+
+
+    render();
+}
+function render() {
+    //camera.lookAt( scene.position );
+    //camera.updateMatrixWorld();
+    renderer.render( scene, camera );
 }
 function onWindowResize() {
     var aspect = window.innerWidth / window.innerHeight;
@@ -89,13 +51,4 @@ function onWindowResize() {
     camera.bottom = - frustumSize / 2;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
-}
-function animate() {
-    requestAnimationFrame( animate );
-    render();
-}
-function render() {
-    //camera.lookAt( scene.position );
-    //camera.updateMatrixWorld();
-    renderer.render( scene, camera );
 }
---
+++ js/primitives.js
@@ -0,0 +1,80 @@
+class Poly{
+    constructor(value){
+        value.position ? this.position = value.position : this.position = new THREE.Vector3(0, 0, 0);
+        value.rotation ? this.rotation = value.rotation : this.rotation = 0;
+        value.scale ? this.scale = value.scale : this.scale = new THREE.Vector3(1, 1, 0);
+        value.color ? this.color = value.color: this.color = 0xffffff;
+    }
+    get mPosition() { return this.position }
+    get mScale() { return this.scale }
+    get mColor() { return this.color }
+
+    set mPosition(position) { this.position = position; }
+    set mScale(scale) { this.scale = scale; }
+    set mColor(color) { this.color = color; }
+
+    create(){ return }
+}
+
+class Square extends Poly{
+    constructor(value){
+        super(value)
+    }
+
+    create(){
+        var geometry = new THREE.PlaneBufferGeometry( 2*this.scale.x, 2*this.scale.y, 0 );
+        var material = new THREE.MeshBasicMaterial( {color: this.color, side: THREE.DoubleSide} );
+        var plane = new THREE.Mesh( geometry, material );
+        plane.name = "Square"
+        plane.rotation.z = this.rotation;
+        plane.position.set(this.position.x, this.position.y, this.position.z)
+        return plane
+    }
+}
+
+class Circle extends Poly{
+    constructor(value){
+        super(value)
+    }
+
+    create(){
+        var geometry = new THREE.CircleBufferGeometry( 1*this.scale.x, 32 );
+        var material = new THREE.MeshBasicMaterial( {color: this.color, side: THREE.DoubleSide} );
+        var circle = new THREE.Mesh( geometry, material );
+        circle.name = "Circle";
+        circle.rotation.z = this.rotation;
+        circle.position.set(this.position.x, this.position.y, this.position.z)
+        return circle
+    }
+}
+
+class Triangle extends Poly{
+    constructor(value){
+        super(value)
+    }
+
+    create(){
+        var ax, ay, bx, by;
+        var cx = 0, r = 1*this.scale.x;
+        var cy = r;
+        bx = cx * Math.cos( 2.0944 ) - ( cy * Math.sin( 2.0944 ) );
+        by = cx * Math.sin( 2.0944 ) + ( cy * Math.cos( 2.0944 ) );
+        ax = cx * Math.cos( 4.18879 ) - ( cy * Math.sin( 4.18879 ) );
+        ay = cx * Math.sin( 4.18879 ) + ( cy * Math.cos( 4.18879 ) );
+
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push( new THREE.Vector3( ax, ay, this.position.z ) );
+        geometry.vertices.push( new THREE.Vector3( bx, by, this.position.z ) );
+        geometry.vertices.push( new THREE.Vector3( cx, cy, this.position.z ) );
+
+        var face = new THREE.Face3( 0, 1, 2, new THREE.Vector3( 0, 1, 0 ) );
+        geometry.faces.push( face )
+
+        var material = new THREE.MeshBasicMaterial( {color: this.color, side: THREE.DoubleSide} );
+        var tri = new THREE.Mesh( geometry, material )
+        tri.name = "Triangle";
+        tri.rotation.z = this.rotation;
+        tri.position.set(this.position.x, this.position.y, this.position.z)
+        return tri
+    }
+}
