9cbaa41a495174066ae95f020599d9bf274ca6f3
Ruger <Ruger4@gmail.com>
2019-01-26 05:56:16 +0000

    gettin places

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var camera, scene, renderer;
+
+/*var camera, scene, renderer;
 var geometry, material, mesh;
+var frustumSize = 1000;
 
 init();
 animate();
 
 function init() {
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = 1;
+    //camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 10 );
+    var aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000 );
+    scene.add( camera )
 
     scene = new THREE.Scene();
 
@@ -14,18 +18,84 @@ function init() {
     material = new THREE.MeshNormalMaterial();
 
     mesh = new THREE.Mesh( geometry, material );
+
+    mesh.position.x = 0.2;
+    mesh.position.y = 0.2;
     scene.add( mesh );
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.getElementById("main").appendChild( renderer.domElement );
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+function onWindowResize() {
+    var aspect = window.innerWidth / window.innerHeight;
+    camera.left = - frustumSize * aspect / 2;
+    camera.right = frustumSize * aspect / 2;
+    camera.top = frustumSize / 2;
+    camera.bottom = - frustumSize / 2;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 function animate() {
     requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    renderer.render( scene, camera );
+}*/
 
+var container, camera, scene, raycaster, renderer;
+var frustumSize = 10;
+
+init();
+animate();
+
+function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+    
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color("rgb(80, 80, 80)");
+
+    var aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 100 );
+    camera.lookAt( scene.position );
+    camera.position.z = 1;
+    camera.updateMatrixWorld();
+
+    var light = new THREE.DirectionalLight( 0xffffff, 1 );
+    light.position.set( 1, 1, 1 ).normalize();
+    scene.add( light );
+
+    var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+    object.position.x = 0;
+    object.position.y = 0;
+    object.position.z = 0;
+    scene.add( object );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+function onWindowResize() {
+    var aspect = window.innerWidth / window.innerHeight;
+    camera.left = - frustumSize * aspect / 2;
+    camera.right = frustumSize * aspect / 2;
+    camera.top = frustumSize / 2;
+    camera.bottom = - frustumSize / 2;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+function animate() {
+    requestAnimationFrame( animate );
+    render();
+}
+function render() {
+    //camera.lookAt( scene.position );
+    //camera.updateMatrixWorld();
     renderer.render( scene, camera );
 }
